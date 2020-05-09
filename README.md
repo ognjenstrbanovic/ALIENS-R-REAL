9060f2a78c407ebded088dccd2fe8e6b9803c4d5
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-05-09 18:51:22 -0400

    completed part 1 💪

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # javascript-challenge
+Objectives
+- Understand JS fundamentals: arrays, conditionals, loops, functions, objects.
+- Understand functional programming with map, forEach.
+- Work with common data structures.
+- Be introduced to data driven documents (d3.js).
+- Understand how to select elements using d3.select.
+- Use d3 for basic DOM manipulation.
+- Understand how to use callbacks.
+- Understand the structure of html tables.
+- Populate a table using static data structures.
+- Understand events.
+- Use d3 to attach events to DOM elements.
+- Dynamically manipulate the DOM through events.
+- Filter data with JavaScript.
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,6 @@
 // Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
 
+
 // from data.js
 var tableData = data;
 
@@ -7,39 +8,72 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Use d3 to append one table row `tr` for each UFO sighting object, 1 cell per UFO sighting value, and update each cell's text with UFO sighting values
-tableData.forEach(function(tableData) {
+function loadData(dataset) {
+    dataset.forEach(function(dataset) {
     var row = tbody.append("tr");
-    Object.entries(tableData).forEach(function([key, value]) {
+    Object.entries(dataset).forEach(function([key, value]) {
         var cell = row.append("td");
         cell.text(value)
+        });
     });
-});
+};
+loadData(tableData);
+
 
 
 // Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
 
-dates = ['1/1/2020', '1/2/2010', '1/3/2010', '1/4/2010', '1/5/2010', '1/6/2010', '1/7/2010', '1/8/2010', '1/9/2010', '1/10/2010', '1/11/2010', '1/12/2010', '1/13/2010'];
 
 // Getting a reference to the button on the page with the id property set to `filter-btn`
-var filterButton = d3.select("#filter-btn");
+// var filterButton = d3.select("#filter-btn");
+var filterButton = document.querySelector("#filter-btn");
+
 // Getting a reference to the input element on the page with the id property set to 'datetime'
-var inputField = d3.select("#datetime");
+var inputElement = document.querySelector("#datetime");
+console.log(tableData);
+
+function handleEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // clear the input value
+    var anonymous = d3.select("#datetime").node().value;
+    console.log(anonymous);
+    d3.select("#datetime").node().value = "";
+};
+
+// fn creating new 'tbody' element, replacing previous by passing in 'array' parameter data
+function loadFilteredData(array) {
+    var oldTBody = document.querySelector("tbody");
+    var newTBody = document.createElement("tbody");
+    array.forEach(function(obj) {
+        var row = newTBody.insertRow();
+        Object.entries(obj).forEach(function([key, value]) {
+            var cell = row.insertCell();
+            cell.innerText = value;
+        });
+    });
+    oldTBody.parentNode.replaceChild(newTBody, oldTBody);
+};
 
 // This function is triggered when the button is clicked
-function handleClick() {
-    console.log("A filter button was clicked!");
+function handleClick(event) {
     // We can use d3 to see the object that dispatched the event
-    console.log(d3.event.target);
+    console.log(event.target.value);
+    // Get the value property of the input element
+    var inputValue = inputElement.value;
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    console.log(inputValue);
+    console.log(filteredData);
+    console.log(JSON.stringify(filteredData));
+    loadFilteredData(filteredData);
 };
 // We can use the `on` function in d3 to attach an event to the function
-filterButton.on("click", handleClick);
+filterButton.addEventListener("click", handleClick);
+d3.select(`#filter-btn`).on("click", handleEnter);
 
 // Input fields can trigger a change event when new text is entered.
-inputField.on("change", function() {
-    var newText = d3.event.target.value;
+var newText;
+inputElement.addEventListener("input", function() {
+    newText = event.target.value;
     console.log(newText);
-});
-filterButton.on("click", function() {
-    // Create a custom filtering function
-    return tableData.filter(sighting.datetime === newText);
 });
